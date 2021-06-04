89fd871ecb4dd791227790cc60418c2589f5d6e9
SH0909 <48480810+SH0909@users.noreply.github.com>
2021-06-05 04:16:11 +0900

    Update __init__.py

--- api/getlist/__init__.py
+++ api/getlist/__init__.py
@@ -8,8 +8,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     #conn_str=os.environ['AZURE_TABLE_CONN_STR']
     #nstr="hello"
     #nstr=nstr+conn_str
-    conn_str=os.environ.get("TEST_KEY")
-    return func.HttpResponse(conn_str)
+    #conn_str=os.environ.get("TEST_KEY")
+    return func.HttpResponse(str(os.environ))
 '''
     table_service_client = TableServiceClient.from_connection_string(conn_str=conn_str)
     table_client = table_service_client.get_table_client(table_name="myTable")
